8c66ebe076346ca3a48711965b8c4d428fda636b
harada <hiromi@snc-jp.com>
2025-06-27 11:05:56 +0900

    fix:言語コード変更

--- src/php/common/header_lp.php
+++ src/php/common/header_lp.php
@@ -4,10 +4,10 @@
       <!-- <button><img src="./img/icon_hum.webp" alt="ハンバーガーメニュー"></button> -->
       <a href="https://tayori.com/faq/9b13b098e2faaa3fcb2f2579bd49c893f9e49ce4/" class="contct_head" target="_blank"><img src="./img/icon_mail.svg" alt="お問い合わせ"></a>
       <div class="lang-menu">
-        <a href="/index.php">JP</a>
-        <a href="/en/index.php">EN</a>
-        <a href="/zh/index.php">ZH</a>
-        <a href="/ko/index.php">KO</a>
+        <a href="/index.php">ja</a>
+        <a href="/en/index.php">en</a>
+        <a href="/zh/index.php">zh</a>
+        <a href="/ko/index.php">ko</a>
       </div>
     </div>
     <a href="/index.php" class="hlogo">
